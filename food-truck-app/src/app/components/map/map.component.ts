db4bc46b67ce7966ae942ebb5b1bfe4d34871069
ejohn156 <ejohn156@uncc.edu>
2019-06-17 15:48:22 -0400

    implemented mapbox and display map in the map component

--- food-truck-app/src/app/components/map/map.component.ts
+++ food-truck-app/src/app/components/map/map.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'
+import { environment } from '../../../environments/environment'
+import { MapboxService } from '../../mapbox.service'
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +10,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  title = 'food-truck-app'
+  map: mapboxgl.Map;
+  constructor() {
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+
+
+  ngOnInit() {
+    mapboxgl.accessToken = environment.mapbox.accessToken;
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: AppComponent.style,
+      center: [-80.84313, 35.2271], // [lng, lat]
+      zoom: 14, // starting zoom
+      scrollZoom: true,
+    });
+    this.map.on('load', (event) => {
+      this.map.addSource('customMarker', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: []
+        }
+
+
+      });
+      const markers = MapboxService.getMarkers();
+      const data = {
+        type: 'FeatureCollection',
+        features: markers
+      };
+      this.map.getSource('customMarker').setData(data);
+
+      this.map.addLayer({
+        id: 'customMarketid',
+        source: 'customMarker',
+        type: 'symbol',
+        layout: {
+          'text-field': '{message}',
+          'text-size': 24,
+          'text-transform': 'uppercase',
+          'icon-image': 'marker-15',
+          'text-offset': [0, 1.5]
+        },
+        paint: {
+          'text-color': '#f16624',
+          'text-halo-color': '#fff',
+          'text-halo-width': 2
+        }
+      });
+    })
+  }
 }
---
+++ food-truck-app/src/app/mapbox.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+
+
+import * as mapboxgl from 'mapbox-gl';
+@Injectable({
+  providedIn: 'root'
+})
+export class MapboxService {
+
+  constructor() {
+    mapboxgl.accessToken = environment.mapbox.accessToken;
+  }
+  static getMarkers() {
+    const geoJson = [{
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': ['80.20929129999999', '13.0569951']
+      },
+      'properties': {
+        'message': 'Chennai'
+      }
+    }, {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Point',
+        'coordinates': ['77.350048', '12.953847']
+      },
+      'properties': {
+        'message': 'bangulare'
+      }
+    }];
+
+
+    return geoJson;
+
+
+  }
+}
